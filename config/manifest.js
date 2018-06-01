215ac44acd518072ab3d39bb777cb8197bb2da91
Guilherme Tassinari <guitassinari@gmail.com>
2018-05-31 21:36:04 -0300

    Add PWA support (#3)

---
+++ config/manifest.js
@@ -0,0 +1,27 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = function(/* environment, appConfig */) {
+  // See https://github.com/san650/ember-web-app#documentation for a list of
+  // supported properties
+
+  return {
+    name: "enactus-team-management-frontend",
+    short_name: "enactus-team-management-frontend",
+    description: "",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+      {
+        src: "/ember-welcome-page/images/construction.png",
+        sizes: "540x540",
+        type: "image/png"
+      }
+    ],
+    ms: {
+      tileColor: '#fff'
+    }
+  };
+}
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,14 +4,23 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    'asset-cache': {
+      include: [
+        'assets/**/*',
+        'ember-welcome-page/images/*'
+      ]
+    },
+    'esw-cache-fallback': {
+      patterns: [ '/' ],
+      version: '1' // Changing the version will bust the cache
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
   // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
+  // environments, specbrew install heroku/brew/herokuify an object as the first parameter. That
   // object's keys should be the environment name and the values
   // should be the asset to use in that environment.
   //
