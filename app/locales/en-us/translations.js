f4f6829075381810766708699d53c6a5cd74de31
Guilherme Tassinari <guitassinari@gmail.com>
2018-06-13 17:19:23 -0300

    Add home route

--- app/locales/en-us/translations.js
+++ app/locales/en-us/translations.js
@@ -1,6 +1,14 @@
 export default {
   "name": "name",
   "email": "e-mail",
+  "password": "password",
+  "password.confirmation": "password confirmation",
+  "home.button.signup": 'Signup',
+  "home.button.signin": "Signin",
   "signup.form.title":  "Signup to add your team!",
-  "signup.form.submit":  "Signup"
+  "signup.form.submit":  "Signup",
+  "signup.form.signin": "ALready a member? Click here to signin",
+  "signin.form.title": "Signin and impact more and more!",
+  "signin.form.submit": "Signin",
+  "signin.form.signup": "Not a member? Click here to signup!"
 };
--- app/locales/pt-br/translations.js
+++ app/locales/pt-br/translations.js
@@ -1,6 +1,14 @@
 export default {
   "name": "nome",
   "email": "e-mail",
+  "password": "senha",
+  "password.confirmation": "confirmação de senha",
+  "home.button.signup": 'Cadastre-se',
+  "home.button.signin": "Entrar",
   "signup.form.title": "Cadastre-se para adicionar a sua equipe!",
-  "signup.form.submit": "Cadastrar"
+  "signup.form.submit": "Cadastrar",
+  "signup.form.signin": "Já tem uma conta? Clique aqui para entrar!",
+  "signin.form.title": "Entre e impacte sempre mais!",
+  "signin.form.submit": "Entrar",
+  "signin.form.signup": "Ainda não tem uma conta? Cadastre-se!"
 };
